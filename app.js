43f6b1c82771f553bd35a42e1da63cd1bcfedc11
anusreesudheer <153237299+anusreesudheer@users.noreply.github.com>
2024-05-01 20:05:52 +0300

    updated LO

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 8000
 const corsOptions ={
-  origin:['https://wonder-of-india-fe.onrender.com','http://localhost:5173'],
+  origin: ['https://wonder-of-india-fe.onrender.com','http://localhost:3000'],
 }
-
+//
 
 // database connection
 const connect =async () =>{
